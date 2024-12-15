99d85c3e91d522b68d682db42b8b51b04c1d0a8f
Ilean Monterrubio Jr <ileanmjr88@gmail.com>
2024-12-14 23:21:41 -0600

    guestbook last check in

--- src/components/GuestSignature.tsx
+++ src/components/GuestSignature.tsx
@@ -2,16 +2,16 @@ import react from '@astrojs/react';
 
 interface GuestSignatureProps {
     name: string;
-    date: string;
-    comment: string;
+    date: Date;
+    message: string;
 };
 
-const GuestSignature = ({name, date, comment} : GuestSignatureProps) => {
+const GuestSignature = ({name, date, message} : GuestSignatureProps) => {
     return (
         <div className='flex justify-center items-center py-4'>
             <h4 className='text-center text-lg text-yellow'>{name}</h4>
             <p className='text-center text-sm text-green'>{date}</p>
-            <p className='text-center text-sm text-blue'>{comment}</p>
+            <p className='text-center text-sm text-blue'>{message}</p>
         </div>
     );
 };
